7a6696bf3ff14befd478b19a2abb053e74ff5201
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-02-20 15:52:06 +0900

    feat(field): introduce `FieldDialect`

---
+++ zkir/Dialect/Field/IR/FieldAttributes.h
@@ -0,0 +1,10 @@
+#ifndef ZKIR_DIALECT_FIELD_IR_FIELDATTRIBUTES_H_
+#define ZKIR_DIALECT_FIELD_IR_FIELDATTRIBUTES_H_
+
+#include "zkir/Dialect/Field/IR/FieldDialect.h"
+#include "zkir/Dialect/Field/IR/FieldTypes.h"
+
+#define GET_ATTRDEF_CLASSES
+#include "zkir/Dialect/Field/IR/FieldAttributes.h.inc"
+
+#endif  // ZKIR_DIALECT_FIELD_IR_FIELDATTRIBUTES_H_
---
+++ zkir/Dialect/Field/IR/FieldTypes.h
@@ -0,0 +1,10 @@
+#ifndef ZKIR_DIALECT_FIELD_IR_FIELDTYPES_H_
+#define ZKIR_DIALECT_FIELD_IR_FIELDTYPES_H_
+
+#include "zkir/Dialect/Field/IR/FieldAttributes.h"
+#include "zkir/Dialect/Field/IR/FieldDialect.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "zkir/Dialect/Field/IR/FieldTypes.h.inc"
+
+#endif  // ZKIR_DIALECT_FIELD_IR_FIELDTYPES_H_
