0af03d05572d2cf9bf5ca805576e049519017846
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-03-07 17:00:19 +0900

    feat(poly): introduce `PrimitiveRootAttr`

--- zkir/Dialect/Poly/IR/PolyAttributes.h
+++ zkir/Dialect/Poly/IR/PolyAttributes.h
@@ -2,6 +2,7 @@
 #define ZKIR_DIALECT_POLY_IR_POLYATTRIBUTES_H_
 
 #include "mlir/Dialect/Polynomial/IR/PolynomialAttributes.h"
+#include "zkir/Dialect/Field/IR/FieldAttributes.h"
 #include "zkir/Dialect/Poly/IR/PolyDialect.h"
 #include "zkir/Dialect/Poly/IR/PolyTypes.h"
 
