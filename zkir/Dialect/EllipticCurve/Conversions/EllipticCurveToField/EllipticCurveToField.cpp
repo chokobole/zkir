d640ce65982e8af4b84a0162bda99025f5f6b2d1
ashjeong <ashjeong@umich.edu>
2025-05-16 19:33:44 +0900

    feat(elliptic_curve): restructure `ScalarMulOp` lowering
    Helper function introduced for MSM op

--- zkir/Dialect/EllipticCurve/Conversions/EllipticCurveToField/EllipticCurveToField.cpp
+++ zkir/Dialect/EllipticCurve/Conversions/EllipticCurveToField/EllipticCurveToField.cpp
@@ -75,13 +75,6 @@ static Type convertXYZZLikeType(ShapedType type) {
   return type;
 }
 
-static Type convertXYZZLikeType(ShapedType type) {
-  if (auto xyzzType = dyn_cast<XYZZType>(type.getElementType())) {
-    return type.cloneWith(type.getShape(), convertXYZZType(xyzzType));
-  }
-  return type;
-}
-
 static Type convertPointLikeType(ShapedType type) {
   Type elementType = type.getElementType();
   if (isa<AffineType>(elementType)) {
@@ -411,6 +404,93 @@ struct ConvertSub : public OpConversionPattern<SubOp> {
 
 // Currently implements Double-and-Add algorithm
 // TODO(ashjeong): implement GLV
+static Value convertScalarMulImpl(Value point, Value scalarPF, Type pointType,
+                                  Type outputType, ImplicitLocOpBuilder b) {
+  auto baseFieldType = cast<field::PrimeFieldType>(scalarPF.getType());
+  unsigned outputBitWidth = baseFieldType.getModulus().getValue().getBitWidth();
+  auto signlessIntType =
+      IntegerType::get(b.getContext(), outputBitWidth, IntegerType::Signless);
+  auto scalar = b.create<field::ExtractOp>(signlessIntType, scalarPF);
+
+  auto zeroPF = b.create<field::ConstantOp>(baseFieldType, 0);
+  SmallVector<Value> zeroes{zeroPF, zeroPF, zeroPF};
+  Value initalPoint;
+
+  if (isa<XYZZType>(outputType)) {
+    zeroes.push_back(zeroPF);
+    initalPoint = point;
+  } else if (isa<AffineType>(pointType)) {
+    initalPoint = convertConvertPointTypeImpl(point, pointType, outputType, b);
+  } else {
+    initalPoint = point;
+  }
+  Value zeroPoint = b.create<tensor::FromElementsOp>(zeroes);
+
+  // `decreasingScalar` set to `scalar` and `result` set to zero point.
+  auto whileOp = b.create<scf::WhileOp>(
+      /*resultTypes=*/
+      TypeRange({signlessIntType, initalPoint.getType(), zeroPoint.getType()}),
+      /*operands=*/
+      ValueRange({scalar, initalPoint, zeroPoint}),
+      /*beforeBuilder=*/
+      [&](OpBuilder &nestedBuilder, Location nestedLoc, ValueRange args) {
+        ImplicitLocOpBuilder b(nestedLoc, nestedBuilder);
+        auto arithZero = b.create<arith::ConstantIntOp>(0, signlessIntType);
+        // if `decreasingScalar` > 0, continue
+        Value decreasingScalar = args[0];
+        auto cmpGt = b.create<arith::CmpIOp>(arith::CmpIPredicate::ugt,
+                                             decreasingScalar, arithZero);
+        b.create<scf::ConditionOp>(cmpGt, args);
+      },
+      /*afterBuilder=*/
+      [&](OpBuilder &nestedBuilder, Location nestedLoc, ValueRange args) {
+        ImplicitLocOpBuilder b(nestedLoc, nestedBuilder);
+        auto arithOne = b.create<arith::ConstantIntOp>(1, signlessIntType);
+        Value decreasingScalar = args[0];
+        Value multiplyingPoint = args[1];
+        Value result = args[2];
+
+        // if `decreasingScalar` % 1 == 1...
+        auto bitAdd = b.create<arith::AndIOp>(decreasingScalar, arithOne);
+        auto cmpEq =
+            b.create<arith::CmpIOp>(arith::CmpIPredicate::eq, bitAdd, arithOne);
+        auto ifOp = b.create<scf::IfOp>(
+            cmpEq,
+            // ...then add `multiplyingPoint` to `result`
+            /*thenBuilder=*/
+            [&](OpBuilder &builder, Location loc) {
+              ImplicitLocOpBuilder b(loc, builder);
+              Value innerResult =
+                  convertAddImpl(result, multiplyingPoint, outputType,
+                                 outputType, outputType, b);
+              b.create<scf::YieldOp>(innerResult);
+            },
+            /*elseBuilder=*/
+            [&](OpBuilder &builder, Location loc) {
+              b.create<scf::YieldOp>(result);
+            });
+        // double `multiplyingPoint`
+        Value doubledPoint =
+            convertDoubleImpl(multiplyingPoint, outputType, outputType, b);
+        // right shift `decreasingScalar` by 1
+        decreasingScalar = b.create<arith::ShRUIOp>(decreasingScalar, arithOne);
+
+        // See here for more info:
+        // https://github.com/iree-org/iree/issues/16956
+        auto multPointBufferOp =
+            b.create<bufferization::MaterializeInDestinationOp>(
+                doubledPoint, multiplyingPoint);
+        auto resultBufferOp =
+            b.create<bufferization::MaterializeInDestinationOp>(
+                ifOp.getResult(0), result);
+
+        b.create<scf::YieldOp>(
+            ValueRange({decreasingScalar, multPointBufferOp.getResult(),
+                        resultBufferOp.getResult()}));
+      });
+  return whileOp.getResult(2);
+}
+
 struct ConvertScalarMul : public OpConversionPattern<ScalarMulOp> {
   explicit ConvertScalarMul(MLIRContext *context)
       : OpConversionPattern<ScalarMulOp>(context) {}
@@ -422,99 +502,15 @@ struct ConvertScalarMul : public OpConversionPattern<ScalarMulOp> {
       ConversionPatternRewriter &rewriter) const override {
     ImplicitLocOpBuilder b(op.getLoc(), rewriter);
 
-    Type inputType = op.getPoint().getType();
+    Value point = adaptor.getPoint();
+    Value scalarPF = op.getScalar();
+
+    Type pointType = op.getPoint().getType();
     Type outputType = op.getOutput().getType();
 
-    Value scalarPF = op.getScalar();
-    auto baseFieldType = cast<field::PrimeFieldType>(scalarPF.getType());
-    unsigned outputBitWidth =
-        baseFieldType.getModulus().getValue().getBitWidth();
-    auto signlessIntType = IntegerType::get(op.getContext(), outputBitWidth,
-                                            IntegerType::Signless);
-    auto scalar = b.create<field::ExtractOp>(signlessIntType, scalarPF);
-
-    Value point = adaptor.getPoint();
-    auto zeroPF = b.create<field::ConstantOp>(baseFieldType, 0);
-    SmallVector<Value> zeroes{zeroPF, zeroPF, zeroPF};
-    Value initalPoint;
-
-    if (isa<XYZZType>(outputType)) {
-      zeroes.push_back(zeroPF);
-      initalPoint = point;
-    } else if (isa<AffineType>(inputType)) {
-      initalPoint =
-          convertConvertPointTypeImpl(point, inputType, outputType, b);
-    } else {
-      initalPoint = point;
-    }
-    Value zeroPoint = b.create<tensor::FromElementsOp>(zeroes);
-
-    // `decreasingScalar` set to `scalar` and `result` set to zero point.
-    auto whileOp = b.create<scf::WhileOp>(
-        /*resultTypes=*/
-        TypeRange(
-            {signlessIntType, initalPoint.getType(), zeroPoint.getType()}),
-        /*operands=*/
-        ValueRange({scalar, initalPoint, zeroPoint}),
-        /*beforeBuilder=*/
-        [&](OpBuilder &nestedBuilder, Location nestedLoc, ValueRange args) {
-          ImplicitLocOpBuilder b(nestedLoc, nestedBuilder);
-          auto arithZero = b.create<arith::ConstantIntOp>(0, signlessIntType);
-          // if `decreasingScalar` > 0, continue
-          Value decreasingScalar = args[0];
-          auto cmpGt = b.create<arith::CmpIOp>(arith::CmpIPredicate::ugt,
-                                               decreasingScalar, arithZero);
-          b.create<scf::ConditionOp>(cmpGt, args);
-        },
-        /*afterBuilder=*/
-        [&](OpBuilder &nestedBuilder, Location nestedLoc, ValueRange args) {
-          ImplicitLocOpBuilder b(nestedLoc, nestedBuilder);
-          auto arithOne = b.create<arith::ConstantIntOp>(1, signlessIntType);
-          Value decreasingScalar = args[0];
-          Value multiplyingPoint = args[1];
-          Value result = args[2];
-
-          // if `decreasingScalar` % 1 == 1...
-          auto bitAdd = b.create<arith::AndIOp>(decreasingScalar, arithOne);
-          auto cmpEq = b.create<arith::CmpIOp>(arith::CmpIPredicate::eq, bitAdd,
-                                               arithOne);
-          auto ifOp = b.create<scf::IfOp>(
-              cmpEq,
-              // ...then add `multiplyingPoint` to `result`
-              /*thenBuilder=*/
-              [&](OpBuilder &builder, Location loc) {
-                ImplicitLocOpBuilder b(loc, builder);
-                Value innerResult =
-                    convertAddImpl(result, multiplyingPoint, outputType,
-                                   outputType, outputType, b);
-                b.create<scf::YieldOp>(innerResult);
-              },
-              /*elseBuilder=*/
-              [&](OpBuilder &builder, Location loc) {
-                b.create<scf::YieldOp>(result);
-              });
-          // double `multiplyingPoint`
-          Value doubledPoint =
-              convertDoubleImpl(multiplyingPoint, outputType, outputType, b);
-          // right shift `decreasingScalar` by 1
-          decreasingScalar =
-              b.create<arith::ShRUIOp>(decreasingScalar, arithOne);
-
-          // See here for more info:
-          // https://github.com/iree-org/iree/issues/16956
-          auto multPointBufferOp =
-              b.create<bufferization::MaterializeInDestinationOp>(
-                  doubledPoint, multiplyingPoint);
-          auto resultBufferOp =
-              b.create<bufferization::MaterializeInDestinationOp>(
-                  ifOp.getResult(0), result);
-
-          b.create<scf::YieldOp>(
-              ValueRange({decreasingScalar, multPointBufferOp.getResult(),
-                          resultBufferOp.getResult()}));
-        });
-
-    rewriter.replaceOp(op, whileOp.getResult(2));
+    Value scalarMul =
+        convertScalarMulImpl(point, scalarPF, pointType, outputType, b);
+    rewriter.replaceOp(op, scalarMul);
     return success();
   }
 };
