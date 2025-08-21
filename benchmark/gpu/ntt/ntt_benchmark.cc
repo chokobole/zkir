dfcd71c3e842bd236a676a40468058904cdee1a8
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-08-21 19:17:49 +0900

    fix(benchmark): isolate gpu computation from mem transfer

--- benchmark/gpu/ntt/ntt_benchmark.cc
+++ benchmark/gpu/ntt/ntt_benchmark.cc
@@ -3,7 +3,9 @@
 #include <random>
 
 #include "benchmark/BenchmarkUtils.h"
+#include "benchmark/CudaUtils.h"
 #include "benchmark/benchmark.h"
+#include "cuda_runtime_api.h" // NOLINT(build/include_subdir)
 #include "gtest/gtest.h"
 #include "mlir/ExecutionEngine/MemRefUtils.h"
 #include "mlir/Support/LLVM.h"
@@ -34,32 +36,59 @@ void fillWithRandom(i64 &elem, ArrayRef<int64_t> coords) {
 
 template <bool kIsGPU>
 void BM_ntt_benchmark(::benchmark::State &state) {
-  OwningMemRef<i64, 1> input(/*shape=*/{NUM_COEFFS}, /*shapeAlloc=*/{},
-                             /*init=*/fillWithRandom);
+  OwningMemRef<i64, 1> hInput(/*shape=*/{NUM_COEFFS}, /*shapeAlloc=*/{},
+                              /*init=*/fillWithRandom);
+  OwningMemRef<i64, 1> hTemp({NUM_COEFFS}, {});
 
-  OwningMemRef<i64, 1> tmp({NUM_COEFFS}, {});
-  for (auto _ : state) {
-    state.PauseTiming();
-    memcpy((*tmp).data, (*input).data, sizeof(i64) * NUM_COEFFS);
-    state.ResumeTiming();
-    if constexpr (kIsGPU) {
-      _mlir_ciface_ntt_gpu(&*tmp);
-    } else {
-      _mlir_ciface_ntt_cpu(&*tmp);
-    }
-  }
+  const size_t bytes = sizeof(i64) * NUM_COEFFS;
 
   if constexpr (kIsGPU) {
-    _mlir_ciface_intt_gpu(&*tmp);
+    auto dInputBuf = makeCudaUnique<i64>(NUM_COEFFS);
+    auto dTmpBuf = makeCudaUnique<i64>(NUM_COEFFS);
+
+    CHECK_CUDA_ERROR(cudaMemcpy(dInputBuf.get(), hInput->data, bytes,
+                                cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+
+    StridedMemRefType<i64, 1> dTmpRef{/*basePtr=*/dTmpBuf.get(),
+                                      /*data=*/dTmpBuf.get(),
+                                      /*offset=*/0,
+                                      /*sizes=*/{NUM_COEFFS},
+                                      /*strides=*/{1}};
+
+    for (auto _ : state) {
+      state.PauseTiming();
+      CHECK_CUDA_ERROR(cudaMemcpy(dTmpBuf.get(), dInputBuf.get(), bytes,
+                                  cudaMemcpyDeviceToDevice));
+      CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+      state.ResumeTiming();
+
+      _mlir_ciface_ntt_gpu(&dTmpRef);
+    }
+
+    _mlir_ciface_intt_gpu(&dTmpRef);
+
+    // Copy back to host for a correctness check
+    CHECK_CUDA_ERROR(
+        cudaMemcpy(hTemp->data, dTmpBuf.get(), bytes, cudaMemcpyDeviceToHost));
+    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
   } else {
-    _mlir_ciface_intt_cpu(&*tmp);
+    for (auto _ : state) {
+      state.PauseTiming();
+      std::memcpy(hTemp->data, hInput->data, bytes);
+      state.ResumeTiming();
+
+      _mlir_ciface_ntt_cpu(&*hTemp);
+    }
+
+    _mlir_ciface_intt_cpu(&*hTemp);
   }
 
   // FIXME(batzor): The NTT benchmark is not working on GPU because the
   // `cuLaunchKernel` fails with `CUDA_ERROR_INVALID_VALUE`.
   if constexpr (!kIsGPU) {
     for (int i = 0; i < NUM_COEFFS; i++) {
-      EXPECT_EQ((*tmp)[i], (*input)[i]);
+      EXPECT_EQ((*hTemp)[i], (*hInput)[i]);
     }
   }
 }
@@ -77,17 +106,17 @@ BENCHMARK_TEMPLATE(BM_ntt_benchmark, /*kIsGPU=*/true)
 // clang-format off
 // NOLINTBEGIN(whitespace/line_length)
 //
-// 2025-08-07T08:20:47+00:00
+// 2025-08-21T10:12:26+00:00
 // Run on AMD Ryzen 9 9950X3D (32 X 624 MHz CPU s)
 // CPU Caches:
 //   L1 Data 48 KiB (x16)
 //   L1 Instruction 32 KiB (x16)
 //   L2 Unified 1024 KiB (x16)
 //   L3 Unified 98304 KiB (x2)
-// Load Average: 24.83, 28.23, 17.45
+// Load Average: 4.21, 3.76, 5.38
 // -----------------------------------------------------
 // Benchmark           Time             CPU   Iterations
 // -----------------------------------------------------
-// ntt_cpu          5.42 ms         5.34 ms          157
+// ntt_cpu          10.4 ms         9.70 ms           71
 // ntt_gpu          ---- ms         ---- ms          ---
 // NOLINTEND()
--- benchmark/gpu/vecops/vecops_benchmark.cc
+++ benchmark/gpu/vecops/vecops_benchmark.cc
@@ -3,7 +3,9 @@
 #include <random>
 
 #include "benchmark/BenchmarkUtils.h"
+#include "benchmark/CudaUtils.h"
 #include "benchmark/benchmark.h"
+#include "cuda_runtime_api.h" // NOLINT(build/include_subdir)
 #include "mlir/ExecutionEngine/MemRefUtils.h"
 #include "mlir/Support/LLVM.h"
 
@@ -35,15 +37,46 @@ void fillWithRandom(i64 &elem, ArrayRef<int64_t> coords) {
 
 template <bool kIsGPU>
 void BM_matvec_benchmark(::benchmark::State &state) {
-  OwningMemRef<i64, 2> mat({NUM_COEFFS, 100}, {}, fillWithRandom);
-  OwningMemRef<i64, 1> vec({100}, {}, fillWithRandom);
-  OwningMemRef<i64, 1> out({NUM_COEFFS}, {}, {});
+  OwningMemRef<i64, 2> hMat({NUM_COEFFS, 100}, {}, fillWithRandom);
+  OwningMemRef<i64, 1> hVec({100}, {}, fillWithRandom);
+  OwningMemRef<i64, 1> hOut({NUM_COEFFS}, {}, {});
 
-  for (auto _ : state) {
-    if constexpr (kIsGPU) {
-      _mlir_ciface_matvec_gpu(&*mat, &*vec, &*out);
-    } else {
-      _mlir_ciface_matvec_cpu(&*mat, &*vec, &*out);
+  const size_t bytesMat = NUM_COEFFS * 100 * sizeof(i64);
+  const size_t bytesVec = 100 * sizeof(i64);
+
+  if constexpr (kIsGPU) {
+    auto dMatBuf = makeCudaUnique<i64>(NUM_COEFFS * 100);
+    auto dVecBuf = makeCudaUnique<i64>(100);
+    auto dOutBuf = makeCudaUnique<i64>(NUM_COEFFS);
+
+    CHECK_CUDA_ERROR(cudaMemcpy(dMatBuf.get(), hMat->data, bytesMat,
+                                cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaMemcpy(dVecBuf.get(), hVec->data, bytesVec,
+                                cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+
+    StridedMemRefType<i64, 2> dMatRef{/*basePtr=*/dMatBuf.get(),
+                                      /*data=*/dMatBuf.get(),
+                                      /*offset=*/0,
+                                      /*sizes=*/{NUM_COEFFS, 100},
+                                      /*strides=*/{100, 1}};
+    StridedMemRefType<i64, 1> dVecRef{/*basePtr=*/dVecBuf.get(),
+                                      /*data=*/dVecBuf.get(),
+                                      /*offset=*/0,
+                                      /*sizes=*/{100},
+                                      /*strides=*/{1}};
+    StridedMemRefType<i64, 1> dOutRef{/*basePtr=*/dOutBuf.get(),
+                                      /*data=*/dOutBuf.get(),
+                                      /*offset=*/0,
+                                      /*sizes=*/{NUM_COEFFS},
+                                      /*strides=*/{1}};
+
+    for (auto _ : state) {
+      _mlir_ciface_matvec_gpu(&dMatRef, &dVecRef, &dOutRef);
+    }
+  } else {
+    for (auto _ : state) {
+      _mlir_ciface_matvec_cpu(&*hMat, &*hVec, &*hOut);
     }
   }
 }
@@ -61,17 +94,17 @@ BENCHMARK_TEMPLATE(BM_matvec_benchmark, /*kIsGPU=*/true)
 // clang-format off
 // NOLINTBEGIN(whitespace/line_length)
 //
-// 2025-08-07T08:22:22+00:00
+// 2025-08-21T10:10:45+00:00
 // Run on AMD Ryzen 9 9950X3D (32 X 624 MHz CPU s)
 // CPU Caches:
 //   L1 Data 48 KiB (x16)
 //   L1 Instruction 32 KiB (x16)
 //   L2 Unified 1024 KiB (x16)
 //   L3 Unified 98304 KiB (x2)
-// Load Average: 6.08, 21.10, 15.97
+// Load Average: 3.69, 3.33, 5.43
 // -----------------------------------------------------
 // Benchmark           Time             CPU   Iterations
 // -----------------------------------------------------
-// matvec_cpu       25.6 ms         25.6 ms           27
-// matvec_gpu       57.9 ms         57.9 ms            9
+// matvec_cpu       32.1 ms         29.6 ms           18
+// matvec_gpu       8.14 ms         8.14 ms           87
 // NOLINTEND()
