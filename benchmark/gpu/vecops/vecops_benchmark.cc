82b020305fe3c7d5b279318e54885edf1bc5ddb5
Batzorig Zorigoo <32958247+batzor@users.noreply.github.com>
2025-08-22 19:05:17 +0900

    fix(benchmark): ensure correctness of GPU vecops
    The `parallelize-affine` is the only reason the benchmark gets very
    slow. Other changes in this commit does not affect the benchmark.

--- benchmark/gpu/vecops/vecops_benchmark.cc
+++ benchmark/gpu/vecops/vecops_benchmark.cc
@@ -6,6 +6,7 @@
 #include "benchmark/CudaUtils.h"
 #include "benchmark/benchmark.h"
 #include "cuda_runtime_api.h" // NOLINT(build/include_subdir)
+#include "gtest/gtest.h"
 #include "mlir/ExecutionEngine/MemRefUtils.h"
 #include "mlir/Support/LLVM.h"
 
@@ -43,6 +44,7 @@ void BM_matvec_benchmark(::benchmark::State &state) {
 
   const size_t bytesMat = NUM_COEFFS * 100 * sizeof(i64);
   const size_t bytesVec = 100 * sizeof(i64);
+  const size_t bytesOut = NUM_COEFFS * sizeof(i64);
 
   if constexpr (kIsGPU) {
     auto dMatBuf = makeCudaUnique<i64>(NUM_COEFFS * 100);
@@ -72,7 +74,26 @@ void BM_matvec_benchmark(::benchmark::State &state) {
                                       /*strides=*/{1}};
 
     for (auto _ : state) {
+      state.PauseTiming();
+      // Reset the output buffer to 0 since the reduction happens on top of
+      // the output buffer.
+      CHECK_CUDA_ERROR(cudaMemset(dOutBuf.get(), 0, NUM_COEFFS * sizeof(i64)));
+      CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+      state.ResumeTiming();
+
       _mlir_ciface_matvec_gpu(&dMatRef, &dVecRef, &dOutRef);
+      CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+    }
+
+    // Copy back to host for a correctness check
+    OwningMemRef<i64, 1> hOutGpu({NUM_COEFFS}, {}, {});
+    CHECK_CUDA_ERROR(cudaMemcpy(hOutGpu->data, dOutBuf.get(), bytesOut,
+                                cudaMemcpyDeviceToHost));
+    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+
+    _mlir_ciface_matvec_cpu(&*hMat, &*hVec, &*hOut);
+    for (int i = 0; i < NUM_COEFFS; i++) {
+      EXPECT_EQ((*hOut)[i], (*hOutGpu)[i]);
     }
   } else {
     for (auto _ : state) {
@@ -94,17 +115,17 @@ BENCHMARK_TEMPLATE(BM_matvec_benchmark, /*kIsGPU=*/true)
 // clang-format off
 // NOLINTBEGIN(whitespace/line_length)
 //
-// 2025-08-21T10:10:45+00:00
-// Run on AMD Ryzen 9 9950X3D (32 X 624 MHz CPU s)
+// 2025-08-22T10:00:03+00:00
+// Run on AMD Ryzen 9 9950X3D (32 X 5550.91 MHz CPU s)
 // CPU Caches:
 //   L1 Data 48 KiB (x16)
 //   L1 Instruction 32 KiB (x16)
 //   L2 Unified 1024 KiB (x16)
 //   L3 Unified 98304 KiB (x2)
-// Load Average: 3.69, 3.33, 5.43
+// Load Average: 0.44, 0.59, 0.73
 // -----------------------------------------------------
 // Benchmark           Time             CPU   Iterations
 // -----------------------------------------------------
-// matvec_cpu       32.1 ms         29.6 ms           18
-// matvec_gpu       8.14 ms         8.14 ms           87
+// matvec_cpu       25.0 ms         24.8 ms           32
+// matvec_gpu        189 ms          189 ms            4
 // NOLINTEND()
